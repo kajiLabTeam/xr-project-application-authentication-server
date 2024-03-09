e8d91245dbc5d3e1abb7006a378f2cab422a3792
Yusei Mizutani <yusei4520030401@gmail.com>
2024-03-10 03:29:39 +0900

    add : エントリーポイントの定義

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/kajiLabTeam/xr-project-application-authentication-server/handlers"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	r := gin.Default()
+
+	handlers.AuthApplicationHandler(r)
+	handlers.RegisterApplicationHandler(r)
+
+	r.Run()
+}
